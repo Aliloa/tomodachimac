4537874f91727841cdb219d7400c6f123c2ea2bd
Marine <marineslimane05@gmail.com>
2026-06-23 13:40:41 +0200

    fixed connexion issue locally

--- modele.py
+++ modele.py
@@ -1,17 +1,15 @@
 import mysql.connector
 
-def initDb():
-    return mysql.connector.connect(
+mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",
+        password="Mistinguette500",
         database="tomodachimac"
-    )
+)
 
 #recup données
 def getData():
     print('---------------------')
-    mydb = initDb()
     mycursor = mydb.cursor(dictionary=True)
     mycursor.execute('''select * from users''')
     result = mycursor.fetchall()
@@ -19,7 +17,6 @@ def getData():
 
 #ajouter données
 def inputData(nom):
-    mydb = initDb()
     mycursor = mydb.cursor(dictionary=True)
     mycursor.execute("INSERT INTO users (nom) VALUES (%s)", (nom,))
     mydb.commit()
@@ -29,14 +26,12 @@ def inputData(nom):
 
 #supprimer données
 def deleteData(delete_id):
-    mydb = initDb()
     mycursor = mydb.cursor(dictionary=True)
     mycursor.execute("DELETE FROM users WHERE id = %s", (delete_id,))
     mydb.commit()
     mycursor.close()
 
 def connexion(pseudo, mdp):
-    mydb = initDb()
     mycursor = mydb.cursor(dictionary=True)
     mycursor.execute("SELECT * FROM users WHERE pseudo = %s AND mdp = %s", (pseudo, mdp))
     return mycursor.fetchone()  # renvoie l'user si trouvé, None sinon
