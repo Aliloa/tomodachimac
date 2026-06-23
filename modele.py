d9ba03306f16968953894c74b112b1b9e901a494
Marine <marineslimane05@gmail.com>
2026-06-23 13:52:08 +0200

    added .env for local database connexion

--- modele.py
+++ modele.py
@@ -1,10 +1,16 @@
 import mysql.connector
+# for .env : 
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# informations of the database depend on local .env
 mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Mistinguette500",
-        database="tomodachimac"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
 )
 
 #recup données
