a07ab54c09cdb4c770e3a9ccc7c999c3ee9c1ffa
Alina <108938418+Aliloa@users.noreply.github.com>
2026-06-24 11:03:33 +0200

    creation de l'ile

--- models/island.py
+++ models/island.py
@@ -6,16 +6,12 @@ def getIsland():
     return cur.fetchall()
 
 def addIsland(islandName, idIsland):
-    cur = mydb.cursor(dictionary=True)
-    cur.execute("SELECT id_ile FROM Ile WHERE nom_ile = %s AND id_compte = %s", (islandName, idIsland))
-    if cur.fetchone():
-        return False
-    cur.execute("INSERT INTO Ile (nom_ile, id_compte, note) VALUES (%s, %s, 0)", (islandName, idIsland))
+    mycursor = mydb.cursor(dictionary=True)
+    mycursor.execute("INSERT INTO Ile (nom_ile, id_compte, note) VALUES (%s, %s, 0)", (islandName, idIsland))
     mydb.commit()
-    return True
+    mycursor.close()
 
 def getAllIslandInformations(islandName):
     cursor = mydb.cursor(dictionary=True)
-
     cursor.execute("SELECT * FROM Ile WHERE id_ile = %s", (islandName))
     return cursor.fetchone() # only one line
--- server.py
+++ server.py
@@ -16,6 +16,10 @@ server.secret_key = os.getenv("SECRET_KEY")
 def connexion():
     return render_template('connexion.html')
 
+@server.route('/profile', methods=['GET'])
+def display_profile():
+    return render_template('profile.html')
+    
 @server.route('/connexion', methods=['POST'])
 def connexion_post():
     pseudo = request.form['pseudo']
@@ -36,21 +40,20 @@ def inscription():
     session['user'] = user
     return render_template('profile.html')
 
-#pour voir le lien du serveur
-if __name__=="__main__":
-    server.run(debug=True)
+@server.route('/create_island', methods=['GET'])
+def display_island_form():
+    return render_template('create_island.html')
 
 @server.route('/create_island', methods=['POST'])
 def add_island():
     if 'user' not in session:
         return redirect('/')
-    name=request.form.get('name','').strip()
+    name = request.form['nom']
     if not name:
         return render_template('create_island.html')
     id_compte = session['user']['id_compte']
-    succes = island.addIsland(name, id_compte)
-    
-    if not succes:
-        return render_template('create_island.html', erreur="Vous avez déjà une île avec ce nom.")
-    
     return redirect('/profile')
+
+#pour voir le lien du serveur
+if __name__=="__main__":
+    server.run(debug=True)
