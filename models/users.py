3399c6fdfb90071c378d0da5b5972623a8072a7e
Alina <108938418+Aliloa@users.noreply.github.com>
2026-06-24 10:12:23 +0200

    base de donnees

--- models/users.py
+++ models/users.py
@@ -4,12 +4,12 @@ import hashlib
 def connexion(pseudo, mdp):
     mdp_hashe = hashlib.sha256(mdp.encode()).hexdigest()
     mycursor = mydb.cursor(dictionary=True)
-    mycursor.execute("SELECT * FROM users WHERE pseudo=%s AND mdp=%s", (pseudo, mdp_hashe))
+    mycursor.execute("SELECT * FROM compte WHERE pseudo=%s AND mdp=%s", (pseudo, mdp_hashe))
     return mycursor.fetchone()
 
 def inscription(pseudo, mdp):
     mdp_hashe = hashlib.sha256(mdp.encode()).hexdigest()
     mycursor = mydb.cursor(dictionary=True)
-    mycursor.execute("INSERT INTO users(pseudo, mdp) VALUES (%s, %s)", (pseudo, mdp_hashe))
+    mycursor.execute("INSERT INTO compte (pseudo, mdp) VALUES (%s, %s)", (pseudo, mdp_hashe))
     mydb.commit()
     mycursor.close()
