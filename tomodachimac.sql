b2fe0c072105cc571ebb37e60aed2f73d85c105b
Alina <108938418+Aliloa@users.noreply.github.com>
2026-06-25 12:13:25 +0200

    BDD

--- tomodachimac.sql
+++ tomodachimac.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 25 juin 2026 à 09:38
+-- Généré le : jeu. 25 juin 2026 à 10:13
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -33,15 +33,15 @@ CREATE TABLE IF NOT EXISTS `compte` (
   `pseudo` varchar(99) NOT NULL,
   `mdp` varchar(99) NOT NULL,
   PRIMARY KEY (`id_compte`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `compte`
 --
 
 INSERT INTO `compte` (`id_compte`, `pseudo`, `mdp`) VALUES
-(1, 'lebron', '119c9ae6f9ca741bd0a76f87fba0b22cab5413187afb2906aa2875c38e213603'),
-(2, 'big yahu', 'b5f044fbe895c22a619df2f398add6cd3e7252a517dac22ecdb27c54089ff7fa');
+(4, 'lebron', '119c9ae6f9ca741bd0a76f87fba0b22cab5413187afb2906aa2875c38e213603'),
+(3, 'cerise', '4ec7c53222c8a758c722e2111541035ce700d5ae7bd0898c5f1b1a743e6450fd');
 
 -- --------------------------------------------------------
 
@@ -69,15 +69,15 @@ CREATE TABLE IF NOT EXISTS `ile` (
   `id_compte` int NOT NULL COMMENT 'cle etrangere',
   `note` int NOT NULL COMMENT 'cle etrangere',
   PRIMARY KEY (`id_ile`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `ile`
 --
 
 INSERT INTO `ile` (`id_ile`, `nom_ile`, `id_compte`, `note`) VALUES
-(2, 'seonde ile de lebronnn yeahhh', 1, 0),
-(3, 'Tel Aviv', 2, 0);
+(5, 'LEBRON ILAND YEAHHH', 4, 0),
+(4, 'IMAC 1', 3, 0);
 
 -- --------------------------------------------------------
 
@@ -100,15 +100,16 @@ CREATE TABLE IF NOT EXISTS `mii` (
   `id_partenaire` int DEFAULT NULL COMMENT '#',
   `id_crush` int DEFAULT NULL COMMENT '#',
   PRIMARY KEY (`id_mii`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `mii`
 --
 
 INSERT INTO `mii` (`id_mii`, `nom_mii`, `age`, `image`, `id_ile`, `sexe`, `personnalite`, `id_famille`, `id_pere`, `id_mere`, `id_partenaire`, `id_crush`) VALUES
-(1, 'lou', 19, NULL, 2, 'feminin', 'Extraverti', NULL, NULL, NULL, NULL, NULL),
-(2, 'Denise', 21, NULL, 2, 'F', 'Timide', NULL, NULL, NULL, NULL, NULL);
+(3, 'Marine', 67, '673cb99d3ee95b481da0c6142d652645.jpg', 4, 'woman', 'reserved', NULL, NULL, NULL, NULL, NULL),
+(4, 'lou', 77, 'images.jpg', 4, 'man', 'outgoing', NULL, NULL, NULL, NULL, NULL),
+(5, 'basketball', 21, '17500.jpg', 5, 'man', 'considerate', NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -122,17 +123,16 @@ CREATE TABLE IF NOT EXISTS `note` (
   `note` int NOT NULL,
   `id_ile` int NOT NULL COMMENT 'cle etrangere',
   PRIMARY KEY (`id_note`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `note`
 --
 
 INSERT INTO `note` (`id_note`, `note`, `id_ile`) VALUES
-(1, 3, 2),
-(2, 0, 1),
-(3, 3, 3),
-(4, 5, 2);
+(7, 3, 5),
+(6, 2, 5),
+(5, 5, 4);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
